899c24fc840bda483cb655afef274a5424db46a5
Brittany Clemmons <brittanyclemmons@gmail.com>
2020-02-29 10:38:22 -0600

    Creating working folder for dashboard html

---
+++ Dashboard/dashboard.js
@@ -0,0 +1,16 @@
+// Create division for dashboard design
+d3.select("body")
+  .insert("div")
+      .attr("class", "dashboard");
+
+// // Insert Sidebar Component
+// d3.select(".dashboard")
+//     .append("div")
+//         .attr("class", "sidebar");
+
+// d3.select(".sidebar")
+//     .append("div")
+//         .attr("class", "bg-light border-right")
+//         .attr("id", "sidebar-wrapper")
+//     .append("div")
+
